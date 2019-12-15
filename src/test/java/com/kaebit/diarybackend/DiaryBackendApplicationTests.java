106798dde49da8698108956965f579f62650ae17
kaebit <kaibee@kakao.com>
2019-12-15 22:15:26 +0900

    Initial commit

---
+++ src/test/java/com/kaebit/diarybackend/DiaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaebit.diarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
